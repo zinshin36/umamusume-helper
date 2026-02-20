e0e706bf1d9bc87fefeecf23f2e70eab8d8509c2
zinshin36 <leewd52@gmail.com>
2026-02-19 21:20:48 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+# Global server APIs (replace with actual working JSON endpoints)
+HORSES_API = "https://umamusu.wiki/api/global-horses.json"
+CARDS_API  = "https://umamusu.wiki/api/global-support-cards.json"
