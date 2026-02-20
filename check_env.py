531bd4888d698061d146fb7c44c47c39b6ab952a
zinshin36 <leewd52@gmail.com>
2026-02-19 23:08:53 -0500

    Create check_env.py

---
+++ check_env.py
@@ -0,0 +1,12 @@
+# check_env.py
+import PySimpleGUI as sg
+
+try:
+    print("Button:", sg.Button)
+    print("Text:", sg.Text)
+    print("Listbox:", sg.Listbox)
+    print("Combo:", sg.Combo)
+    print("PySimpleGUI environment is OK")
+except AttributeError as e:
+    print("PySimpleGUI environment is broken")
+    print(e)
