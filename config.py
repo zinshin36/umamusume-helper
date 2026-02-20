eb01c24d55563482a96315ce4bf8773c2a31fe61
zinshin36 <leewd52@gmail.com>
2026-02-20 10:33:53 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-# config.py
-
 # Global Wiki pages (EN)
 HORSES_PAGE = "https://umamusu.wiki/w/Category:Trainees?tab=EN"
 CARDS_PAGE  = "https://umamusu.wiki/w/Category:Support_Cards?tab=EN"
 
+# Local cache
 CACHE_FILE = "data/cache.json"
