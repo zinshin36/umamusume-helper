29063f07252469b8c869d4dac02b7386ad9d6965
zinshin36 <leewd52@gmail.com>
2026-02-21 09:41:12 -0500

    Update recommend.py

--- utils/recommend.py
+++ utils/recommend.py
@@ -1,24 +1,6 @@
-import json
-
-ARCHETYPE_FILE = "data/archetypes.json"
-
-def load_archetypes():
-    try:
-        with open(ARCHETYPE_FILE, "r") as f:
-            return json.load(f)
-    except:
-        return {}
-
 def recommend_inheritance(horse):
-    archetypes = load_archetypes()
-
-    archetype = "General"
-    if horse["id"] in archetypes:
-        archetype = archetypes[horse["id"]]["type"]
-
     return {
         "horse": horse["name"],
         "version": "Default",
-        "recommended_skill": "Speed Boost",
-        "archetype": archetype
+        "recommended_skill": "Speed Boost"
     }
