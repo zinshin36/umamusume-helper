5594b5f4a0bc4f2c57ee6282c5c99971d314eb48
zinshin36 <leewd52@gmail.com>
2026-02-23 08:01:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-import logging
-import os
-from gui import UmaGui
-from data_manager import ensure_directories
-
-def setup_logging():
-    os.makedirs("logs", exist_ok=True)
-
-    logging.basicConfig(
-        filename="logs/app.log",
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s",
-    )
+from gui import run
 
 if __name__ == "__main__":
-    ensure_directories()
-    setup_logging()
-
-    logging.info("Application started")
-
-    app = UmaGui()
-    app.run()
+    run()
