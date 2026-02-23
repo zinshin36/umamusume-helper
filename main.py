08da74bd43504c7acd8df296c4ea34516d965ec8
zinshin36 <leewd52@gmail.com>
2026-02-23 07:19:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-import os
 from pathlib import Path
 import logging
 
-from utils.fetch import fetch_all_sites
 from gui import App
 
-# Create folders
 Path("data/images").mkdir(parents=True, exist_ok=True)
 Path("logs").mkdir(parents=True, exist_ok=True)
 
-# Logging
 logging.basicConfig(
     filename="logs/app.log",
     level=logging.INFO,
