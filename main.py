b7409ce3eebed80891f77f41e88e46193b0dbbd4
zinshin36 <leewd52@gmail.com>
2026-02-24 01:28:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import logging
 from gui import start_gui
 
-if __name__ == "__main__":
-    logging.basicConfig(
-        filename="app.log",
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s"
-    )
+logging.basicConfig(
+    filename="app.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
+if __name__ == "__main__":
     logging.info("Application started")
     start_gui()
