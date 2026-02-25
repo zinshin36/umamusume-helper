d0dc6655c2598c307e89b7dd75726b96b8a623bd
zinshin36 <leewd52@gmail.com>
2026-02-25 17:56:43 -0500

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,30 +1,22 @@
 import json
 import os
 
-
 DATA_FILE = "data.json"
-IMAGE_FOLDER = "data/images/support"
 
 
 class DataManager:
 
-    def __init__(self):
-        self.ensure_folders()
-
-    def ensure_folders(self):
-        os.makedirs(IMAGE_FOLDER, exist_ok=True)
-
     def save(self, horses, supports):
-        self.ensure_folders()
+
+        os.makedirs("data/images/support", exist_ok=True)
+
         with open(DATA_FILE, "w", encoding="utf-8") as f:
-            json.dump(
-                {"horses": horses, "supports": supports},
-                f,
-                indent=2
-            )
+            json.dump({
+                "horses": horses,
+                "supports": supports
+            }, f, indent=2)
 
     def load(self):
-        self.ensure_folders()
 
         if not os.path.exists(DATA_FILE):
             return [], []
